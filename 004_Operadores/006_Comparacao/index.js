37185bfe926607f8536e8615a0c5a6bba5e42bd4
Van Faria <57229209+TearG@users.noreply.github.com>
2023-02-25 00:31:04 +0000

    Adicionadas Pastas 006, 007 e 008 com as devidas aulas de JavaScript.

---
+++ 004_Operadores/006_Comparacao/index.js
@@ -0,0 +1,23 @@
+//console.log(1 == 1);
+//console.log(1 == "1");
+//console.log(1 === "1");
+//console.log(1 === 1);
+
+//console.log(1 != 2);
+//console.log(1 != "1");
+//console.log(1 !== 1);
+
+//console.log(1 > 2);
+//console.log(3 > 2);
+//console.log(3 < 2);
+
+//console.log(1 >= 1);
+//console.log(3 >= 2);
+
+//console.log(2 >= 2);
+//console.log(2 <= 2);
+//console.log(1 <= 2);
+
+/*
+  == , === , != , !==, >, <, >=, <=
+*/
