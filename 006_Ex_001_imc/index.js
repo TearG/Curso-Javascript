121dfbf584feff382a25327a35c000880a8ad344
Van Faria <57229209+TearG@users.noreply.github.com>
2023-02-28 18:07:15 +0000

    Práticas: Calculo IMC e Tabuada.

---
+++ 006_Ex_001_imc/index.js
@@ -0,0 +1,30 @@
+const peso = 100;
+const altura = 1.7;
+const imc = Number((peso / (altura * altura)).toFixed(2));
+
+console.log(imc);
+switch (true){
+  case imc < 17:
+    console.log("Muito abaixo do peso");
+    break;
+
+  case imc >= 17 && imc <= 18.49:
+    console.log("Abaixo do peso");
+    break;
+
+  case imc >= 18.5 && imc <= 24.99:
+   console.log("Peso Normal");
+   break;
+
+  case imc >= 25 && imc <= 29.99:
+   console.log("Acima do Peso");
+   break;
+
+  case imc >= 30 && imc <= 34.99:
+   console.log("Obesidade I");
+   break;
+
+  case imc >= 35 && imc <= 39.99:
+   console.log("Obesidade II");
+   break;
+}
---
+++ 006_Ex_002_tabuada/index.js
@@ -0,0 +1,10 @@
+const multiplicador = 8;
+
+//let i = 0
+// chegará até o valor desejado = 10
+// ele add + 1 sobre o valor então sempre que acabar o código ele add + 1 = 1 + 1 = 2
+for(let i = 0; i <= 100; i++) {
+  const calc = multiplicador * i;
+  console.log(`${multiplicador} x ${i} = ${calc}`);
+}
+  
